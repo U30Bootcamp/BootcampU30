29fd67681f3112bef6c226048ec3ef9ca8401402
kadiryunusdemir <73827115+kadiryunusdemir@users.noreply.github.com>
2023-06-26 14:05:38 +0300

    Ölüm için collider'lar ayarlandı

--- GalaktikToplayici/Assets/Scripts/MovementController.cs
+++ GalaktikToplayici/Assets/Scripts/MovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,13 +11,14 @@ public class MovementController : MonoBehaviour
     
     private Rigidbody _playerRigidbody;
     private Vector2 _mMove;
-    private bool isFalling;
+    private bool _isFalling;
+    private bool _isDead;
     
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            Jump();
+            Jump(3.01f, 1.5f);
         }
     }
     
@@ -38,39 +40,31 @@ public class MovementController : MonoBehaviour
         if (_playerRigidbody.velocity.y < 0f)
         {
             _playerRigidbody.AddForce(Vector3.down * fallRate);
-            isFalling = true;
+            _isFalling = true;
         }
     }
 
-    // normale göre 1.5 kat ve yerden daha yukarıdayken zıplar
-    private void Jump()
+    private void Jump(float maxHeightFromObject, float jumpScale)
     {
-        bool raycastHit = Physics.Raycast(transform.position, Vector3.down, 3.01f);
+        if (_isDead)
+            return;
+        
+        bool raycastHit = ShootRaycast(maxHeightFromObject);
 
         // eğer karakter yerde ise zıplat
-        if (raycastHit && isFalling)
+        if (raycastHit && _isFalling)
         {
-            isFalling = false;
-            float jumpForce = Mathf.Sqrt(jumpHeight * jumpHeight * Physics.gravity.y * -2f) * _playerRigidbody.mass * 1.5f;
-            _playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
-    }
-    
-    private void MoveJump()
-    {
-        bool raycastHit = Physics.Raycast(transform.position, Vector3.down, 1.01f);
-
-        // eğer karakter yerde ise zıplat
-        if (raycastHit && isFalling)
-        {
-            isFalling = false;
-            float jumpForce = Mathf.Sqrt(jumpHeight * jumpHeight * Physics.gravity.y * -2f) * _playerRigidbody.mass;
+            _isFalling = false;
+            float jumpForce = Mathf.Sqrt(jumpHeight * jumpHeight * Physics.gravity.y * -2f) * _playerRigidbody.mass * jumpScale;
             _playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
     
     private void Move(Vector2 direction)
     {
+        if (_isDead)
+            return;    
+
         if (direction == Vector2.zero)
             return;
 
@@ -84,6 +78,32 @@ public class MovementController : MonoBehaviour
         Vector3.ClampMagnitude(velocityChange, maxForce);
         
         _playerRigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
-        MoveJump(); // astronot yürüyüşü için zımplama ekler
+        Jump(1.01f, 1f); // astronot yürüyüşü için zıplama ekler
+    }
+
+    private bool ShootRaycast(float maxHeightFromObject)
+    {
+        bool raycastHit = Physics.Raycast(transform.position, Vector3.down, maxHeightFromObject);
+        return raycastHit;
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("game over UI");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            _isDead = true;
+            _playerRigidbody.AddForce(Vector3.back * 10f, ForceMode.Impulse);
+            GameOver();
+        }
+        else if (other.gameObject.layer == LayerMask.NameToLayer("FallLimit"))
+        {
+            _isDead = true;
+            GameOver();
+        }
     }
 }
